aa7e7863042f3ff725c56d282e5a42b16853e468
Nik Malhotra <nmal@me.com>
2019-02-10 19:37:05 +0000

    response callback now working

--- https_example.js
+++ https_example.js
@@ -1,4 +1,22 @@
-var https = require('https');
+const https = require('https');
 
 console.log('did it');
 
+
+// http request:
+
+ const options = {
+     host: 'www.nikmal.co',
+     path: '/'
+ };
+
+ const callback = (response) => {
+    console.log('In response handler callback!');
+    console.log('Response:', response);
+ };
+
+ console.log('Making request!');
+
+ https.request(options,callback).end();
+
+ console.log('I\'ve made the request!');
